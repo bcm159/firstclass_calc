e66bc6a7a1b46b998e250fb2e4ff7154eb504867
jangjihyuk <bcm159@naver.com>
2022-09-15 16:35:39 +0900

    insert

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ function App() {
       var month = ("0" + (1 + date.getMonth())).slice(-2);
       var day = ("0" + date.getDate()).slice(-2);
       var result_date = year + month + day;
-      var url ='korea/site/program/financial/exchangeJSON?authkey=MXuMd56F9xZeEkKrd13R9QUOKSsEYLpa&searchdate='+result_date+'&data=AP01';
+      // var url ='korea/site/program/financial/exchangeJSON?authkey=MXuMd56F9xZeEkKrd13R9QUOKSsEYLpa&searchdate='+result_date+'&data=AP01';
+      var url ='https://www.koreaexim.go.kr/korea/site/program/financial/exchangeJSON?authkey=MXuMd56F9xZeEkKrd13R9QUOKSsEYLpa&searchdate='+result_date+'&data=AP01';
       return url;
       
     }
